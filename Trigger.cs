bf8dc536cf504e3f079b0bffa8e6fd86d096deee
Rémi Salomon <35302284+SalomonRemi@users.noreply.github.com>
2019-05-05 17:09:24 +0200

    First version of the tool + Documentation

---
+++ Trigger.cs
@@ -0,0 +1,413 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+using System;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] // make a private vatriable accessible in inspector, still private
+    private bool canBeTriggeredMultipleTimes;
+    private bool hasBeenTriggered;
+
+    [HideInInspector] // hide a public variable in inspector (even hide in inspector debug mode)
+    public int detectionTypeIndex = 0;
+    [HideInInspector]
+    public List<string> detectionType = new List<string>(new string[] { "On Enter", "On Exit" });
+
+
+    [HideInInspector]
+    public int entitySelectionIndex = 0; // to track list index
+    [HideInInspector]
+    public List<string> entitySelection = new List<string>(new string[] { "By Tag", "By Name", "Specific" }); //list of selection type (availble for dropdown list in editor script)
+    [HideInInspector]
+    public string tagString;
+    [HideInInspector]
+    public string nameString;
+    [HideInInspector]
+    public GameObject specificObject;
+
+    
+    [HideInInspector]
+    public int triggeredEventIndex = 0;
+    [HideInInspector]
+    public List<string> triggeredEvent = new List<string>(new string[] { "Play Sound", "Play Animation", "Play Method from External", "Play Method from Other" }); //Play Method from Other was previously Play Method from Internal
+    [HideInInspector]
+    public AudioClip sound;
+    [HideInInspector]
+    public AudioSource soundSource;
+    [HideInInspector]
+    public Animator animator;
+    [HideInInspector]
+    public string animatorTrigger;
+    [HideInInspector]
+    public MonoBehaviour methodScript;
+    [HideInInspector]
+    public string methodName;
+    [HideInInspector]
+    public string className;
+    [HideInInspector]
+    public string internMethodName;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(canBeTriggeredMultipleTimes)
+        {
+            if (detectionTypeIndex == 0) //0=enter / 1=exit / 2=stay
+            {
+                if (entitySelectionIndex == 0 && other.gameObject.CompareTag(tagString))//by tag
+                {
+                    if (triggeredEventIndex == 0)
+                    {
+                        soundSource.PlayOneShot(sound); // play the sound
+                    }
+                    else if (triggeredEventIndex == 1)
+                    {
+                        animator.SetTrigger(animatorTrigger); // the the animation
+                    }
+                    else if (triggeredEventIndex == 2)
+                    {
+                        methodScript.Invoke(methodName, 0f); // invoke method
+                    }
+                    else if (triggeredEventIndex == 3)
+                    {
+                        MonoBehaviour[] _scripts = other.GetComponents<MonoBehaviour>(); //grab list of every monobehavior in other, then check if one of them name's is similar to our variable name
+
+                        foreach (MonoBehaviour s in _scripts)
+                        {
+                            if(s.GetType().Name == className)
+                            {
+                                s.Invoke(internMethodName, 0f); // if similar, invoke method 
+                            }
+                        }
+                    }
+                }
+                else if (entitySelectionIndex == 1 && other.gameObject.name == nameString) //by name
+                {
+                    if (triggeredEventIndex == 0)
+                    {
+                        soundSource.PlayOneShot(sound);
+                    }
+                    else if (triggeredEventIndex == 1)
+                    {
+                        animator.SetTrigger(animatorTrigger);
+                    }
+                    else if (triggeredEventIndex == 2)
+                    {
+                        methodScript.Invoke(methodName, 0f);
+                    }
+                    else if (triggeredEventIndex == 3)
+                    {
+                        MonoBehaviour[] _scripts = other.GetComponents<MonoBehaviour>();
+
+                        foreach (MonoBehaviour s in _scripts)
+                        {
+                            if (s.GetType().Name == className)
+                            {
+                                s.Invoke(internMethodName, 0f);
+                            }
+                        }
+                    }
+                }
+                else if (entitySelectionIndex == 2 && other.gameObject == specificObject) //specific
+                {
+                    if (triggeredEventIndex == 0)
+                    {
+                        soundSource.PlayOneShot(sound);
+                    }
+                    else if (triggeredEventIndex == 1)
+                    {
+                        animator.SetTrigger(animatorTrigger);
+                    }
+                    else if (triggeredEventIndex == 2)
+                    {
+                        methodScript.Invoke(methodName, 0f);
+                    }
+                    else if (triggeredEventIndex == 3)
+                    {
+                        MonoBehaviour[] _scripts = other.GetComponents<MonoBehaviour>();
+
+                        foreach (MonoBehaviour s in _scripts)
+                        {
+                            if (s.GetType().Name == className)
+                            {
+                                s.Invoke(internMethodName, 0f);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else if (!canBeTriggeredMultipleTimes && !hasBeenTriggered)
+        {
+            if (detectionTypeIndex == 0) //0=enter / 1=exit / 2=stay
+            {
+                if (entitySelectionIndex == 0 && other.gameObject.CompareTag(tagString))//by tag
+                {
+                    if (triggeredEventIndex == 0)
+                    {
+                        soundSource.PlayOneShot(sound);
+                    }
+                    else if (triggeredEventIndex == 1)
+                    {
+                        animator.SetTrigger(animatorTrigger);
+                    }
+                    else if (triggeredEventIndex == 2)
+                    {
+                        methodScript.Invoke(methodName, 0f);
+                    }
+                    else if (triggeredEventIndex == 3)
+                    {
+                        MonoBehaviour[] _scripts = other.GetComponents<MonoBehaviour>();
+
+                        foreach (MonoBehaviour s in _scripts)
+                        {
+                            if (s.GetType().Name == className)
+                            {
+                                s.Invoke(internMethodName, 0f);
+                            }
+                        }
+                    }
+                }
+                else if (entitySelectionIndex == 1 && other.gameObject.name == nameString) //by name
+                {
+                    if (triggeredEventIndex == 0)
+                    {
+                        soundSource.PlayOneShot(sound);
+                    }
+                    else if (triggeredEventIndex == 1)
+                    {
+                        animator.SetTrigger(animatorTrigger);
+                    }
+                    else if (triggeredEventIndex == 2)
+                    {
+                        methodScript.Invoke(methodName, 0f);
+                    }
+                    else if (triggeredEventIndex == 3)
+                    {
+                        MonoBehaviour[] _scripts = other.GetComponents<MonoBehaviour>();
+
+                        foreach (MonoBehaviour s in _scripts)
+                        {
+                            if (s.GetType().Name == className)
+                            {
+                                s.Invoke(internMethodName, 0f);
+                            }
+                        }
+                    }
+                }
+                else if (entitySelectionIndex == 2 && other.gameObject == specificObject) //specific
+                {
+                    if (triggeredEventIndex == 0)
+                    {
+                        soundSource.PlayOneShot(sound);
+                    }
+                    else if (triggeredEventIndex == 1)
+                    {
+                        animator.SetTrigger(animatorTrigger);
+                    }
+                    else if (triggeredEventIndex == 2)
+                    {
+                        methodScript.Invoke(methodName, 0f);
+                    }
+                    else if (triggeredEventIndex == 3)
+                    {
+                        MonoBehaviour[] _scripts = other.GetComponents<MonoBehaviour>();
+
+                        foreach (MonoBehaviour s in _scripts)
+                        {
+                            if (s.GetType().Name == className)
+                            {
+                                s.Invoke(internMethodName, 0f);
+                            }
+                        }
+                    }
+                }
+
+                hasBeenTriggered = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (canBeTriggeredMultipleTimes)
+        {
+            if (detectionTypeIndex == 1) //0=enter / 1=exit / 2=stay
+            {
+                if (entitySelectionIndex == 0 && other.gameObject.CompareTag(tagString))//by tag
+                {
+                    if (triggeredEventIndex == 0)
+                    {
+                        soundSource.PlayOneShot(sound);
+                    }
+                    else if (triggeredEventIndex == 1)
+                    {
+                        animator.SetTrigger(animatorTrigger);
+                    }
+                    else if (triggeredEventIndex == 2)
+                    {
+                        methodScript.Invoke(methodName, 0f);
+                    }
+                    else if (triggeredEventIndex == 3)
+                    {
+                        MonoBehaviour[] _scripts = other.GetComponents<MonoBehaviour>();
+
+                        foreach (MonoBehaviour s in _scripts)
+                        {
+                            if (s.GetType().Name == className)
+                            {
+                                s.Invoke(internMethodName, 0f);
+                            }
+                        }
+                    }
+                }
+                else if (entitySelectionIndex == 1 && other.gameObject.name == nameString) //by name
+                {
+                    if (triggeredEventIndex == 0)
+                    {
+                        soundSource.PlayOneShot(sound);
+                    }
+                    else if (triggeredEventIndex == 1)
+                    {
+                        animator.SetTrigger(animatorTrigger);
+                    }
+                    else if (triggeredEventIndex == 2)
+                    {
+                        methodScript.Invoke(methodName, 0f);
+                    }
+                    else if (triggeredEventIndex == 3)
+                    {
+                        MonoBehaviour[] _scripts = other.GetComponents<MonoBehaviour>();
+
+                        foreach (MonoBehaviour s in _scripts)
+                        {
+                            if (s.GetType().Name == className)
+                            {
+                                s.Invoke(internMethodName, 0f);
+                            }
+                        }
+                    }
+                }
+                else if (entitySelectionIndex == 2 && other.gameObject == specificObject) //specific
+                {
+                    if (triggeredEventIndex == 0)
+                    {
+                        soundSource.PlayOneShot(sound);
+                    }
+                    else if (triggeredEventIndex == 1)
+                    {
+                        animator.SetTrigger(animatorTrigger);
+                    }
+                    else if (triggeredEventIndex == 2)
+                    {
+                        methodScript.Invoke(methodName, 0f);
+                    }
+                    else if (triggeredEventIndex == 3)
+                    {
+                        MonoBehaviour[] _scripts = other.GetComponents<MonoBehaviour>();
+
+                        foreach (MonoBehaviour s in _scripts)
+                        {
+                            if (s.GetType().Name == className)
+                            {
+                                s.Invoke(internMethodName, 0f);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else if (!canBeTriggeredMultipleTimes && !hasBeenTriggered)
+        {
+            if (detectionTypeIndex == 1) //0=enter / 1=exit / 2=stay
+            {
+                if (entitySelectionIndex == 0 && other.gameObject.CompareTag(tagString))//by tag
+                {
+                    if (triggeredEventIndex == 0)
+                    {
+                        soundSource.PlayOneShot(sound);
+                    }
+                    else if (triggeredEventIndex == 1)
+                    {
+                        animator.SetTrigger(animatorTrigger);
+                    }
+                    else if (triggeredEventIndex == 2)
+                    {
+                        methodScript.Invoke(methodName, 0f);
+                    }
+                    else if (triggeredEventIndex == 3)
+                    {
+                        MonoBehaviour[] _scripts = other.GetComponents<MonoBehaviour>();
+
+                        foreach (MonoBehaviour s in _scripts)
+                        {
+                            if (s.GetType().Name == className)
+                            {
+                                s.Invoke(internMethodName, 0f);
+                            }
+                        }
+                    }
+                }
+                else if (entitySelectionIndex == 1 && other.gameObject.name == nameString) //by name
+                {
+                    if (triggeredEventIndex == 0)
+                    {
+                        soundSource.PlayOneShot(sound);
+                    }
+                    else if (triggeredEventIndex == 1)
+                    {
+                        animator.SetTrigger(animatorTrigger);
+                    }
+                    else if (triggeredEventIndex == 2)
+                    {
+                        methodScript.Invoke(methodName, 0f);
+                    }
+                    else if (triggeredEventIndex == 3)
+                    {
+                        MonoBehaviour[] _scripts = other.GetComponents<MonoBehaviour>();
+
+                        foreach (MonoBehaviour s in _scripts)
+                        {
+                            if (s.GetType().Name == className)
+                            {
+                                s.Invoke(internMethodName, 0f);
+                            }
+                        }
+                    }
+                }
+                else if (entitySelectionIndex == 2 && other.gameObject == specificObject) //specific
+                {
+                    if (triggeredEventIndex == 0)
+                    {
+                        soundSource.PlayOneShot(sound);
+                    }
+                    else if (triggeredEventIndex == 1)
+                    {
+                        animator.SetTrigger(animatorTrigger);
+                    }
+                    else if (triggeredEventIndex == 2)
+                    {
+                        methodScript.Invoke(methodName, 0f);
+                    }
+                    else if (triggeredEventIndex == 3)
+                    {
+                        MonoBehaviour[] _scripts = other.GetComponents<MonoBehaviour>();
+
+                        foreach (MonoBehaviour s in _scripts)
+                        {
+                            if (s.GetType().Name == className)
+                            {
+                                s.Invoke(internMethodName, 0f);
+                            }
+                        }
+                    }
+                }
+
+                hasBeenTriggered = true;
+            }
+        }
+    }
+}
---
+++ TriggerEditor.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Trigger))]
+public class TriggerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Trigger _script = (Trigger)target; //get the right class for the editor custom 
+
+        EditorGUILayout.Space(); // add a small space between ui text
+        GUIContent _detectionTypeGUI = new GUIContent("Detection Type"); //initialize the new GUI stuff
+        _script.detectionTypeIndex = EditorGUILayout.Popup(_detectionTypeGUI, _script.detectionTypeIndex, _script.detectionType.ToArray());
+
+        EditorGUILayout.Space(); // add a small space between ui text
+
+        GUIContent _entitySelectionGUI = new GUIContent("Entity Selection"); //initialize the new GUI stuff 
+        _script.entitySelectionIndex = EditorGUILayout.Popup(_entitySelectionGUI, _script.entitySelectionIndex, _script.entitySelection.ToArray()); //create the dropdown list
+        if (_script.entitySelectionIndex == 0) // if it is the first index in this list
+        {
+            _script.tagString = EditorGUILayout.TextField("Tag Name",_script.tagString); //create a text field which is editable from inspector
+        }
+        else if (_script.entitySelectionIndex == 1)
+        {
+            _script.nameString = EditorGUILayout.TextField("Objects Name", _script.nameString);
+        }
+        else if (_script.entitySelectionIndex == 2)
+        {
+            _script.specificObject = (GameObject) EditorGUILayout.ObjectField("Specific Object", _script.specificObject, typeof(GameObject), true); //create a gameobject field which is editable from inspector
+        }
+
+        EditorGUILayout.Space(); // add a small space between ui text
+
+        GUIContent _triggeredEventGUI = new GUIContent("Triggered Event"); //initialize the new GUI stuff
+        _script.triggeredEventIndex = EditorGUILayout.Popup(_triggeredEventGUI, _script.triggeredEventIndex, _script.triggeredEvent.ToArray());
+        if (_script.triggeredEventIndex == 0)
+        {
+            _script.sound = (AudioClip)EditorGUILayout.ObjectField("Sound Clip", _script.sound, typeof(AudioClip), true); // EditorGUILayout.ObjectField can basically be used for any type of variable/classes 
+            _script.soundSource = (AudioSource)EditorGUILayout.ObjectField("Sound Source", _script.soundSource, typeof(AudioSource), true);
+        }
+        else if (_script.triggeredEventIndex == 1)
+        {
+            _script.animator = (Animator)EditorGUILayout.ObjectField("Animator", _script.animator, typeof(Animator), true);
+            _script.animatorTrigger = EditorGUILayout.TextField("Animator Trigger Name", _script.animatorTrigger);
+        }
+        else if (_script.triggeredEventIndex == 2)
+        {
+            _script.methodScript = (MonoBehaviour)EditorGUILayout.ObjectField("Method Script", _script.methodScript, typeof(MonoBehaviour), true);
+            _script.methodName = EditorGUILayout.TextField("Method Name", _script.methodName);
+        }
+        else if (_script.triggeredEventIndex == 3)
+        {
+            _script.className = EditorGUILayout.TextField("Class Name", _script.className);
+            _script.internMethodName = EditorGUILayout.TextField("Method Name", _script.internMethodName);
+        }
+    }
+}
